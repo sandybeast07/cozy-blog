ccc3d90c3fc65671ccd4bb98352da0533a3e5b2e
sandybeast07 <sandybeast.tech@gmail.com>
2026-02-15 20:36:46 +0530

    fixes

--- components/ui/Main.tsx
+++
@@ -1,9 +0,0 @@
-
-import { Post } from '@/lib/types'
-import PostCard from '@/components/PostCard'
-import PostEditor from '@/components/PostEditor'
-export default function Main(){
-    return(
-        
-    )
-}
